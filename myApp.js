5e7c5d5f99f9be3bd97e97899bf1fb012ec43ae4
kavitappatil <patil.poem@gmail.com>
2023-01-11 07:39:17 +0000

    get request

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+app.get("/", function (request, response) {
+  response.send("Hello World.");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
